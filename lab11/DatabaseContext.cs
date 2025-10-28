be5613e598d5f382e2a733059d36e004e5e43aa5
pixiemays <kondakov2007.andrey@gmail.com>
2025-10-28 15:36:01 +0700

    add class DatabaseContext.cs

---
+++ lab11/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿namespace lab11;
+
+using Microsoft.EntityFrameworkCore;
+
+public class DatabaseContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=database.db");
+    }
+}
